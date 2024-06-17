3dfe9fbb724346bdae666f84cbb24b345e064856
Your Name <you@example.com>
2024-06-17 22:42:28 +0300

    final-project

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/education-circle/',
+  // base: '/education-circle-hupa/',
   plugins: [
     Vue({
       template: { transformAssetUrls }
